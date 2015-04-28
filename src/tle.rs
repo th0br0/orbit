be26ef7ab18a72fd846be4288e2681cc35123188
Andreas C. Osowski <andreas.chr.osowski@gmail.com>
2015-04-28 15:56:39 +0200

    Made TLE struct members public Fixed a fix_string bug Added missing trim()'s

--- src/tle.rs
+++ src/tle.rs
@@ -4,37 +4,35 @@ use std::convert::From;
 use std::num::{ParseIntError, ParseFloatError};
 use std::fs::File;
 use std::error::Error;
-use std::str::Lines;
 use std::io::Read;
-use std::slice::Chunks;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct TLE {
     // line 1
-    name: String,
+    pub name: String,
 
-    satellite_number: i16,
-    classification: Classification,
-    id_launch_year: i8,
-    id_launch_number: i16,
-    id_launch_piece: String,
-    epoch_year: i16,
-    epoch: f32,
-    mean_motion_d: f32,
-    mean_motion_dd: f32,
-    bstar: f32,
-    set_number: i32,
+    pub satellite_number: i16,
+    pub classification: Classification,
+    pub id_launch_year: i8,
+    pub id_launch_number: i16,
+    pub id_launch_piece: String,
+    pub epoch_year: i16,
+    pub epoch: f32,
+    pub mean_motion_d: f32,
+    pub mean_motion_dd: f32,
+    pub bstar: f32,
+    pub set_number: i32,
 
     // line 2
-    inclination: f32,
-    right_ascension: f32,
-    eccentricity: f32,
-    perigree: f32,
-    mean_anomaly: f32,
-    mean_motion: f32,
-    revolution_number: i32,
+    pub inclination: f32,
+    pub right_ascension: f32,
+    pub eccentricity: f32,
+    pub perigree: f32,
+    pub mean_anomaly: f32,
+    pub mean_motion: f32,
+    pub revolution_number: i32,
 
-    is_valid: bool
+    valid: bool
 }
 
 #[derive(Clone,PartialEq,Debug)]
@@ -56,13 +54,18 @@ impl From<ParseIntError> for DeserializationError {
     fn from(err: ParseIntError) -> DeserializationError {DeserializationError::ParseError(err.description().to_string())}
 }
 
+// FIXME - this function is scary
 fn fix_string(s: String) -> String {
     if s.starts_with("-.") { return s.replace("-.", "-0.") };
+
+    if s.starts_with(".") {
+        return "0".to_string() + &s;
+    }
 
     match s.find("-") {
         Some(0) => return "-0.".to_string() + &s[1..(s.len() - 2)].to_string() + "e" + &s[(s.len() - 2)..s.len()],
         Some(_) => return "0.".to_string() + &s[0..(s.len() - 2)].to_string() + "e" + &s[(s.len() - 2)..s.len()],
-        None => return format!("0.{}", s)
+        None => return format!("0.{}", s).replace("+0", "e+0")
     }
 }
 
@@ -76,6 +79,7 @@ fn line_checksum(line: String) -> bool {
 }
 
 impl TLE {
+    pub fn is_valid(&self) -> bool { self.valid }
 
     pub fn new(input: &String) -> Result<TLE, DeserializationError> {
         //TODO can we somehow enforce the parameter to have a fixed length?
@@ -99,7 +103,7 @@ impl TLE {
 
             epoch_year: try!(line1[18..20].parse::<i16>()),
             epoch: try!(line1[20..32].trim().parse::<f32>()),
-            mean_motion_d: try!(fix_string(line1[33..43].to_string()).parse::<f32>().map(|v| v * 2.0)),
+            mean_motion_d: try!(fix_string(line1[33..43].trim().to_string()).parse::<f32>().map(|v| v * 2.0)),
             mean_motion_dd: try!(fix_string(line1[44..52].trim().to_string()).parse::<f32>().map(|v| v * 6.0)),
             bstar: try!(fix_string(line1[53..61].trim().to_string()).parse::<f32>()),
             set_number: try!(line1[64..68].trim().parse::<i32>()),
@@ -111,9 +115,9 @@ impl TLE {
             perigree: try!(line2[34..42].trim().parse::<f32>()),
             mean_anomaly: try!(line2[43..51].trim().parse::<f32>()),
             mean_motion: try!(line2[52..63].trim().parse::<f32>()),
-            revolution_number: try!(line2[63..68].parse::<i32>()),
+            revolution_number: try!(line2[63..68].trim().parse::<i32>()),
 
-            is_valid: line_checksum(line1.to_string()) && line_checksum(line2.to_string())
+            valid: line_checksum(line1.to_string()) && line_checksum(line2.to_string())
         };
 
         // Maybe return Err if is_valid = false?
@@ -137,6 +141,8 @@ pub fn parse_file(file: &mut File) -> Vec<TLE> {
     let chunks : Vec<String> = lines.chunks(3).map(|chunk| chunk.iter().fold(String::new(), |acc, c| acc + c.trim() + "\n")).collect();
 
     // FIXME - we're dropping invalid TLEs silently!!!
+    //
+
     chunks.iter().map(|c| TLE::new(c)).filter(|t| t.is_ok()).map(|t| t.unwrap()).collect()
 }
 
@@ -150,7 +156,12 @@ mod test {
     #[test]
     fn test_deserialize_tle() {
         let t = super::TLE::new(&DATA.to_string());
-        assert!(t.is_ok() && t.unwrap().is_valid);
+        assert!(t.is_ok() && t.unwrap().is_valid());
+    }
+
+    #[test]
+    fn test_fix_string() {
+
     }
 
     #[test]
